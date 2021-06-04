92133bce09cfdb2d622707c57f254c81985f4c8f
Foxar <mostek3pl@gmail.com>
2021-06-04 15:50:48 +0200

    Moved additional form fields to a separate component file.

---
+++ src/components/AdditionalForms/AdditionalForms.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Paper, TextField, Typography, Select, MenuItem, Button } from '@material-ui/core'; 
+
+export function SpicyForm(props){
+    console.log("Render spicyform");
+    return(
+        <div className="additionals">
+        <TextField name="spicyness" className="field" variant="filled" value={props.spicyScale} type="number" onChange={props.handleSpicyChange} placeholder="Spicyness scale"/>
+      </div>
+    );
+}
+
+export function SandwichForm(props){
+    console.log("Render sandwichform");
+    return(
+      <div className="additionals">
+        <TextField name="sandwichSlices" className="field" variant="filled" value={props.sandwichSlices} type="number" onChange={props.handleSandwichSliceChange} placeholder="Number of slices"/>
+      </div>
+    );
+}
+
+export function PizzaForm(props){
+    console.log("Render pizzaform");
+    return(
+    <div className="additionals">
+        <TextField name="pizzaSlices" className="field" value={props.pizzaSlices} onChange={props.handlePizzaSliceChange} variant="filled" type="number" placeholder="Number of slices"/>
+        <TextField name="diameter" className="field" value={props.diameter} onChange={props.handleDiameterChange} variant="filled" type="number" step="0.01" placeholder="Diameter"/>
+      </div>);
+}
--- src/components/DishesForm.js
+++ src/components/DishesForm.js
@@ -1,35 +1,41 @@
 
 import './../styles/DishesForm.css';
 import React from 'react';
-import { Paper, TextField, Typography, Select, MenuItem, Button } from '@material-ui/core';
+import { Paper, TextField, Typography, Select, MenuItem, Button } from '@material-ui/core'; 
+import { SpicyForm, PizzaForm, SandwichForm } from './AdditionalForms/AdditionalForms';
+
 
 class DishesForm extends React.Component
 {
   constructor(props){
     super(props);
     this.state={
-      selectedDuration: null,
+      name:"",
+      time:15,
+      diameter: 15,
+      sandwichSlices:  4,
+      pizzaSlices: 6,
       selectedType: null,
-      spicyScale: null,
-      slices: null
     };
-    this.handleTimeChange = this.handleTimeChange.bind(this);
-    this.handleSelectChange = this.handleSelectChange.bind(this);
+    
     this.handleSpicyChange = this.handleSpicyChange.bind(this);
-    this.handleSliceChange = this.handleSliceChange.bind(this);
+    this.handleFormChange = this.handleFormChange.bind(this);
+    this.handleSelectChange = this.handleSelectChange.bind(this);
   }
 
-  handleTimeChange(e){
-    console.log(e);
-    this.setState({
-      selectedDuration: e
-    })
+  handleFormChange(e){
+    let formNames=[
+      "name","time","spicyness","slices","diameter"
+    ];
+
+    if(formNames.includes(e.target.name)){
+      this.setState({
+        [e.target.name]: e.target.value
+      });
+    }
+    console.log(this.state);
   }
-  handleSelectChange(e){
-    this.setState({
-      selectedType: e.target.value
-    })
-  }
+
   handleSpicyChange(e){
     if(e.target.value > 10){
       this.setState({
@@ -45,49 +51,31 @@ class DishesForm extends React.Component
       })
     }
   }
-
-  handleSliceChange(e){
+  handleSelectChange(e){
     this.setState({
-      slices: e.target.value
+      selectedType: e.target.value
     })
   }
-
   render(){
-    console.log("Selected " + this.state.selectedType);
-    let additionalForm;
-    if(this.state.selectedType == 1){
-      additionalForm = 
-      <div className="additionals">
-        <TextField className="field" variant="filled" type="number" placeholder="Number of slices"/>
-        <TextField className="field" variant="filled" type="number" step="0.01" placeholder="Diameter"/>
-      </div>
-    } else if (this.state.selectedType == 2){
-      additionalForm = 
-      <div className="additionals">
-        <TextField className="field" variant="filled" value={this.state.spicyScale} type="number" onChange={this.handleSpicyChange} placeholder="Spicyness scale"/>
-      </div>
-    } else if (this.state.selectedType == 3){
-      additionalForm =
-      <div className="additionals">
-        <TextField className="field" variant="filled" value={this.state.slices} type="number" onChange={this.handleSliceChange} placeholder="Number of slices"/>
-      </div>
-    }
+    let additionalForms =[
+      <PizzaForm diameter={this.state.diameter} handlePizzaSliceChange={this.handleFormChange} handlePizzaDiameterChange={this.handleFormChange}/>,
+      <SpicyForm spicyScale={this.state.spicyScale} handleSpicyChange={this.handleSpicyChange}/>,
+      <SandwichForm sandwichSlices={this.state.sandwichSlices} hadleSandwichSliceChange={this.handleFormChange}/>
+    ];
     
-
-
     return(
       <Paper className="dishesForm">
         <Typography variant='h4'>Please fill out the following data:</Typography>
-        <TextField className="field" variant="filled" name="name" placeholder="Name"/>
+        <TextField className="field" variant="filled" name="name" placeholder="Name" value={this.state.name} onChange={this.handleFormChange}/>
         <Typography variant='subtitle1'>Preparation duration</Typography>
-        <TextField className="field" variant="filled" name="name" type="text" onChange={this.handleTimeChange}/>
+        <TextField className="field" variant="filled" name="time" type="text" value={this.state.time} onChange={this.handleFormChange}/>
         <Select onChange={this.handleSelectChange}>
-          <MenuItem value={1}>Pizza</MenuItem>
-          <MenuItem value={2}>Soup</MenuItem>
-          <MenuItem value={3}>Sandwich</MenuItem>
+          <MenuItem value={0}>Pizza</MenuItem>
+          <MenuItem value={1}>Soup</MenuItem>
+          <MenuItem value={2}>Sandwich</MenuItem>
         </Select>
-        {additionalForm}
-        <Button clasName="submitButton" variant="contained">Submit</Button>
+        {this.state.selectedType && additionalForms[this.state.selectedType]}
+        <Button className="submitButton" variant="contained">Submit</Button>
 
       </Paper>
     );
