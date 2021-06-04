fbf3e8f1fb0e0ac3021722f73032b140eec22812
Foxar <mostek3pl@gmail.com>
2021-06-04 16:02:00 +0200

    Fixed min and max error. General cleanup of code.

--- src/components/AdditionalForms/AdditionalForms.js
+++ src/components/AdditionalForms/AdditionalForms.js
@@ -14,7 +14,7 @@ export function SandwichForm(props){
     console.log("Render sandwichform");
     return(
       <div className="additionals">
-        <TextField name="sandwichSlices" className="field" variant="filled" value={props.sandwichSlices} type="number" onChange={props.handleSandwichSliceChange} placeholder="Number of slices"/>
+        <TextField name="sandwichSlices" className="field" variant="filled" value={props.sandwichSlices} type="number" onChange={props.handleChange} placeholder="Number of slices"/>
       </div>
     );
 }
@@ -23,7 +23,7 @@ export function PizzaForm(props){
     console.log("Render pizzaform");
     return(
     <div className="additionals">
-        <TextField name="pizzaSlices" className="field" value={props.pizzaSlices} onChange={props.handlePizzaSliceChange} variant="filled" type="number" placeholder="Number of slices"/>
-        <TextField name="diameter" className="field" value={props.diameter} onChange={props.handleDiameterChange} variant="filled" type="number" step="0.01" placeholder="Diameter"/>
+        <TextField name="pizzaSlices" className="field" value={props.pizzaSlices} onChange={props.handleChange} variant="filled" type="number" placeholder="Number of slices"/>
+        <TextField name="diameter" className="field" value={props.diameter} onChange={props.handleChange} variant="filled" type="number" step="0.01" placeholder="Diameter"/>
       </div>);
 }
--- src/components/DishesForm.js
+++ src/components/DishesForm.js
@@ -27,7 +27,6 @@ class DishesForm extends React.Component
     let formNames=[
       "name","time","spicyness","slices","diameter"
     ];
-
     if(formNames.includes(e.target.name)){
       this.setState({
         [e.target.name]: e.target.value
@@ -37,19 +36,10 @@ class DishesForm extends React.Component
   }
 
   handleSpicyChange(e){
-    if(e.target.value > 10){
-      this.setState({
-        spicyScale: 10
-      })
-    } else if (e.target.value < 1){
-      this.setState({
-        spicyScale: 1
-      })
-    } else {
-      this.setState({
-        spicyScale: e.target.value
-      })
-    }
+    let spicy = Math.max(1,Math.min(e.target.value),10);
+    this.setState({
+      spicyScale: spicy
+    });
   }
   handleSelectChange(e){
     this.setState({
@@ -58,17 +48,29 @@ class DishesForm extends React.Component
   }
   render(){
     let additionalForms =[
-      <PizzaForm diameter={this.state.diameter} handlePizzaSliceChange={this.handleFormChange} handlePizzaDiameterChange={this.handleFormChange}/>,
+      <PizzaForm diameter={this.state.diameter} handleChange={this.handleFormChange} />,
       <SpicyForm spicyScale={this.state.spicyScale} handleSpicyChange={this.handleSpicyChange}/>,
-      <SandwichForm sandwichSlices={this.state.sandwichSlices} hadleSandwichSliceChange={this.handleFormChange}/>
+      <SandwichForm sandwichSlices={this.state.sandwichSlices} handleChange={this.handleFormChange}/>
     ];
     
     return(
       <Paper className="dishesForm">
         <Typography variant='h4'>Please fill out the following data:</Typography>
-        <TextField className="field" variant="filled" name="name" placeholder="Name" value={this.state.name} onChange={this.handleFormChange}/>
+        <TextField 
+          className="field"
+          variant="filled"
+          name="name"
+          placeholder="Name"
+          value={this.state.name}
+          onChange={this.handleFormChange}/>
         <Typography variant='subtitle1'>Preparation duration</Typography>
-        <TextField className="field" variant="filled" name="time" type="text" value={this.state.time} onChange={this.handleFormChange}/>
+        <TextField 
+          className="field"
+          variant="filled"
+          name="time"
+          type="text"
+          value={this.state.time}
+          onChange={this.handleFormChange}/>
         <Select onChange={this.handleSelectChange}>
           <MenuItem value={0}>Pizza</MenuItem>
           <MenuItem value={1}>Soup</MenuItem>
